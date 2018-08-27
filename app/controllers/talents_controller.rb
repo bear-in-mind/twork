888960b03890493d39b4bf9e23d98d213e3f2146
Julien Maisonnier <julien_maisonnier@yahoo.com>
2018-08-27 18:57:52 +0200

    udpated talents controller a second time

---
+++ app/controllers/talents_controller.rb
@@ -0,0 +1,41 @@
+class TalentsController < ApplicationController
+  def new
+    # authorize @talent
+    @profile = Profile.find(params[:profile_id])
+    @talent = Talent.new
+  end
+
+  def create
+    # authorize @talent
+    @talent = Talent.New(talent_params)
+    if @talent.save
+      redirect_to user_path(@talent)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # authorize @current_user if @talent == current_user
+    @talent = Talent.find(params[:id])
+  end
+
+  def update
+    # authorize current_user
+    current_user.update(user_params)
+    redirect_to user_path(params[:id])
+  end
+
+  def destroy
+    # authorize @talent
+    @talent.destroy
+    redirect_to(user_path(params[:id]))
+  end
+
+  private
+
+  def talent_params
+    params.require(:talent).permit(:skill_id)
+  end
+
+end
