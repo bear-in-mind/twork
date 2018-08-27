885546f0951693622a7b198727520d30fcc0a4b1
Charlotte Pages <pgs.charlotte@gmail.com>
2018-08-27 16:41:37 +0200

    created profiles controller, implemented edit and update, added routes for edit, update, destroy, added associations and GENRES table to User model, missing views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   mount_uploader :avatar, AvatarUploader
-   mount_uploader :banner, BannerUploader
+  mount_uploader :avatar, AvatarUploader
+  mount_uploader :banner, BannerUploader
+
+  has_many :messages
+  has_many :projects
+
+  GENRES = %w[Pop R&B Hip-Hop Rap Rock Electronic EDM Funk Disco House Techno Classical Jazz Folk Soundtrack Traditional]
+  validates :genre, inclusion: { in: GENRES, allow_blank: true }
 end
+
+# Pour les views:
+# User::GENRES
+# f.select as collection
