00b0212621df8b011177b414a53fe063eb7031a7
Louis Sommer <louis.sommer@gmail.com>
2018-08-27 16:17:07 +0200

    Seeded users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_130156) do
+ActiveRecord::Schema.define(version: 2018_08_27_140509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_130156) do
     t.text "bio"
     t.string "genres"
     t.string "main_occupation"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 #
 
-# Seed Instrument Skills
+GENRES = %w(Pop R&B Hip-Hop Rap Rock Electronic EDM Funk Disco House Techno Classical Jazz Folk Soundtrack Traditional)
 
+# Seed Instrument Skills
 Skill.create(name: "Bass")
 Skill.create(name: "Drums")
 Skill.create(name: "Keyboards")
@@ -112,4 +113,93 @@ Skill.create(name: "Music Teacher")
 Skill.create(name: "Music Therapist")
 Skill.create(name: "Musicologist")
 
+# Users
+a = User.new(
+  first_name: "Maxime",
+  last_name: "Garoute",
+  city: "Paris",
+  email: "max@twork.xyz",
+  password: "123456",
+  main_job: "Drummer",
+  genres: 'Pop, Funk, Disco',
+  bio: "I'm a French drummer with deep roots in 80s american culture. I was very influenced by artists like Peter Gabriel, Toto, Michael Jackson and Price. My band is called Venice!"
+)
+a.remote_avatar_url = "https://res.cloudinary.com/twerk/image/upload/v1534855650/profile_pictures/maxime.jpg"
+a.remote_banner_url = "https://res.cloudinary.com/twerk/image/upload/v1534930591/banner_images/max_banner.jpg"
+a.save
 
+b = User.new(
+  first_name: "Nicolas",
+  last_name: "Bauguil",
+  city: "Paris",
+  email: "nico@twork.xyz",
+  password: "123456",
+  main_job: "Guitarist",
+  bio: "I'm a French guitar player. I play all guitars, all genres: I'm as much a fan of Michael Jackson as AC/DC !",
+  genres: 'Funk, Disco, Rock, Pop'
+)
+
+b.remote_avatar_url = "https://res.cloudinary.com/twerk/image/upload/v1534855650/profile_pictures/bogue.jpg"
+b.remote_banner_url = "https://res.cloudinary.com/twerk/image/upload/v1534930591/banner_images/bogue_banner.jpg"
+b.save
+
+c = User.new(
+  first_name: "Louis",
+  last_name: "Sommer",
+  city: "Paris",
+  email: 'louis.sommer@gmail.com',
+  password: "123456",
+  main_job: "Producer",
+  genres: "Pop, Disco, Electro, RnB, Classical",
+  bio: "I'm a musician, arranger and producer from Paris. Deeply influenced by black music, jazz, and electronic music, I will adapt to any project, in any genre, provided I'll have the right to make it groove!"
+)
+
+c.remote_avatar_url = "https://res.cloudinary.com/twerk/image/upload/v1534855789/profile_pictures/louis.jpg"
+c.remote_banner_url = "https://res.cloudinary.com/twerk/image/upload/v1534930591/banner_images/max_banner.jpg"
+c.save
+
+d = User.new(
+  first_name: "Benjamin",
+  last_name: "Diamond",
+  city: "Paris",
+  email: "benj@twork.xyz",
+  password: "123456",
+  main_job: "Artist",
+  bio: "I'm a songwriter, composer, producer and vocalist. Got famous with my band Stardust with Thomas Bangalter. Open to any vocals featuring mostly on Electronic tracks.",
+  genres: "Electro, House, Pop"
+)
+
+d.remote_avatar_url = "https://res.cloudinary.com/twerk/image/upload/v1534855842/profile_pictures/BenjaminDiamond.jpg"
+d.save
+
+e = User.new(
+  first_name: "Johann",
+  last_name: "Dalgaard",
+  city: "Paris",
+  email: "johann@twork.xyz",
+  password: "123456",
+  main_job: "Keyboardist",
+  genres: "Pop, Blues, Rock, Funk",
+  bio: "Hey ! I'm a keyboard player from Denmark. I'm a huge blues fan, and am quite skilled with the organ. I can ply any style and love pop as well!"
+)
+
+e.remote_avatar_url = "https://res.cloudinary.com/twerk/image/upload/v1534855790/profile_pictures/johan.jpg"
+e.remote_banner_url = "https://res.cloudinary.com/twerk/image/upload/v1534930591/banner_images/johan_banner.jpg"
+e.save
+
+f = User.new(
+  first_name: "Sebastien",
+  last_name: "Teti",
+  city: "Los Angeles",
+  email: "seb@twork.xyz",
+  password: "123456",
+  main_job: "Mixing Engineer",
+  genres: "Electro, Pop",
+  bio: "I'm a front-of-house engineer, as well as a very skilled mixer. I especially like electronic music."
+)
+
+f.remote_avatar_url = "https://res.cloudinary.com/twerk/image/upload/v1534855789/profile_pictures/sebastien.jpg"
+f.remote_banner_url = "https://res.cloudinary.com/twerk/image/upload/v1534930591/banner_images/seb_banner.jpg"
+f.save
+
+# Talents
