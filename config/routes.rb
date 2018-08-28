5d8266cb007252e4c348f00f2b2c111dba5f12f7
Julien Maisonnier <julien_maisonnier@yahoo.com>
2018-08-28 11:34:42 +0200

    update with modified routes post merge master

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,16 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :profiles, only: [:edit, :update, :destroy]
-  resources :talents, only: [:new, :create, :edit, :update, :destroy]  
+  resources :talents, only: [:new, :create, :edit, :update, :destroy]
   resources :projects, only: [:index, :show, :new, :create, :edit, :update] do
     resources :tracks, only: [:index, :new, :create, :edit, :update]
   end
-  
+
   resources :tracks, only: [:show] do
     resources :sessions, only: [:new, :create, :destroy] do
       resources :audio_files, only: [:new, :create, :destroy]
     end
+    resources :messages, only: [:new, :create, :destroy]
   end
 
 end
