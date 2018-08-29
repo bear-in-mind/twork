4b579a2ac38bb57682cda89bdfc5ba4e2abb7d45
Louis Sommer <louis.sommer@gmail.com>
2018-08-29 15:54:21 +0200

    player working

--- app/models/audio_file.rb
+++ app/models/audio_file.rb
@@ -1,7 +1,6 @@
 class AudioFile < ApplicationRecord
   belongs_to :uploaded_by, class_name: 'User', foreign_key: :user_id
   belongs_to :session
-  belongs_to :track
 
   mount_uploader :audio, AudioUploader
 end
