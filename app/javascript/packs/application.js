3fcd048d05c8869f7ecc9fdf51b208bf6a4f90dd
Louis Sommer <louis.sommer@gmail.com>
2018-08-29 14:30:50 +0200

    started implementing player

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,24 @@
 import "bootstrap";
+import $ from "jquery";
 
-import { initPlayer } from '../plugins/soundmanager2';
+import { initPlayer } from '../plugins/init_howler';
 
-initPlayer();
+
+let session_tracks = [
+    {
+      title: 'Dark Wagon - Basic Track',
+      howl: null,
+      url: 'https://res.cloudinary.com/twerk/video/upload/v1535531527/audio/Dark_Wagon.m4a'
+    },
+    {
+      title: 'Luce - Basse/Batt',
+      howl: null,
+      url: 'https://res.cloudinary.com/twerk/video/upload/v1535539370/audio/Ciao_BASS-BATT-RHODES.mp3'
+    },
+];
+
+initPlayer(session_tracks);
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
