084deb8c5d632a016a0086cadad6e179dc4f5915
Claire Ziemendorf <claire.ziemendorf@gmail.com>
2018-08-30 19:47:02 +0200

    messagerie pas commencee mais autres modifs

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   belongs_to :track
 
-  validates :content, presence: true
+  validates :content, :track_id, :user_id, presence: true
 end
