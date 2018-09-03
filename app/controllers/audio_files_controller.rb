094abfc8e0a339426bc8a6f9f682cb252fa7030b
Louis Sommer <louis.sommer@gmail.com>
2018-09-03 10:39:04 +0200

    track loading repaired, upload repaired

--- app/controllers/audio_files_controller.rb
+++ app/controllers/audio_files_controller.rb
@@ -6,15 +6,16 @@ class AudioFilesController < ApplicationController
 
   def create
     @audio_file = AudioFile.new(audio_file_params)
+    @audio_file.session_id = params[:session_id]
     timestamp = Date.today.strftime("%e%m")
-    @audio_file.session = Session.find_by(params[:id])
     @audio_file.uploaded_by = current_user
-    @audio_file.name = "#{@audio_file.session.track.name} - #{@audio_file.session.talent.skill.name} - #{timestamp} "
+    @audio_file.name = "#{@audio_file.session.talent.skill.name} - #{timestamp} "
 
     if @audio_file.save
       redirect_to track_path(params[:track_id])
       flash[:notice] = "File uploaded !"
     else
+      # raise
       redirect_to track_path(params[:track_id])
       flash[:alert] = "File upload failed !"
     end
