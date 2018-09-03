ec240ec5cc11bc101452e1e9b060ad14cf3c021a
Louis Sommer <louis.sommer@gmail.com>
2018-09-03 19:48:53 +0200

    changed a few front things

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import { initPlayers } from '../plugins/init_howler';
 import { alertFadeOut } from '../components/alert';
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip({ boundary: 'viewport' })
 })
 
 initPlayers();
