312d4b04a27dad9d8e988e8640b05d837e21467f
Claire Ziemendorf <claire.ziemendorf@gmail.com>
2018-09-05 11:02:56 +0200

    accordeon working

---
+++ app/javascript/components/buttonupdown.js
@@ -0,0 +1,25 @@
+function openCloseDivs() {
+  const sessionInstances = document.querySelectorAll(".session-instances");
+  if (sessionInstances) {
+    sessionInstances.forEach((session) => {
+      console.log(session.dataset.id);
+      openCloseDiv(session.dataset.id);
+    })
+  }
+}
+
+
+function openCloseDiv(sessionId) {
+   $(document).ready(function() {
+   // Hide the div
+    $(`#reveal-${sessionId}`).hide();
+    $(`.rv_button-${sessionId}`).click(function(e){
+      e.preventDefault();$(`#reveal-${sessionId}`).slideToggle();
+      $(`.rv_button-${sessionId}`).toggleClass('opened closed');
+      $(`.hidden-comments-${sessionId}, .comments-displayed-${sessionId}`).toggleClass('hidden')
+    });
+  });
+}
+
+export { openCloseDivs };
+
